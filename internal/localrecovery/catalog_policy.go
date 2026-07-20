4473c6ccc60c1de86bd1246eaf3ee3d960970158
knownothing20 <137740359+knownothing20@users.noreply.github.com>
2026-07-20 21:50:54 +0800

    feat: define catalog state transitions

---
+++ internal/localrecovery/catalog_policy.go
@@ -0,0 +1,19 @@
+package localrecovery
+
+func validCatalogTransition(from, to SnapshotStatus) bool {
+	if from == to {
+		return true
+	}
+	switch from {
+	case SnapshotPending:
+		return to == SnapshotStaged || to == SnapshotInvalid || to == SnapshotRecoveryRequired
+	case SnapshotStaged:
+		return to == SnapshotVerified || to == SnapshotInvalid || to == SnapshotRecoveryRequired
+	case SnapshotVerified:
+		return to == SnapshotInvalid || to == SnapshotRecoveryRequired
+	case SnapshotRecoveryRequired:
+		return to == SnapshotStaged || to == SnapshotInvalid
+	default:
+		return false
+	}
+}
